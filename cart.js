bd39f98f82b9058d42cc648771de80b197b38107
Acaraga <7573131@gmail.com>
2018-04-10 00:32:22 +0300

    Cart + - added

--- cart.js
+++ cart.js
@@ -32,16 +32,23 @@ function showCart() {
             var out = '<h1>Корзина</h1>';
             for (var id in cart) {
                 out += `<button data-id="${id}" class="del-goods">x</button>`;
-                out += `<img src="/img/${goods[id].img}">`;
-                out += ` ${goods[id].name  }`;
-                out += ` ${cart[id]  }`;
-                out += '<br>';
+                out += `<img src="img/${goods[id].img}">`;
+                out += ` ${goods[id].name  } : `;
+                out += `<button data-id="${id}" class="dec-goods">-</button> ${cart[id]  }`;
+                out += `<button data-id="${id}" class="inc-goods">+</button> <br>`;
             }
+            out += '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=- <br>'
+            out += '<a href=".">На главную</a>'
+
+            console.log(cart);
             $('.main-cart').html(out);
             $('.del-goods').on('click', delGoods);
+            $('.dec-goods').on('click', decGoods);
+            $('.inc-goods').on('click', incGoods);
+
         //};
     }
-}
+};
 
 function delGoods() {
     //удаляем товар из корзины
@@ -49,12 +56,33 @@ function delGoods() {
     delete cart[id];
     saveCart();
     showCart();
-}
+};
+function decGoods() {
+    //удаляем товар из корзины
+    var id = $(this).attr('data-id');
+    if (cart[id] < 1) {
+      delete cart[id];
+        saveCart();
+        showCart();
+    } else {
+      cart[id]--;
+      saveCart();
+      showCart();  
+    }
+
+};
+function incGoods() {
+    //удаляем товар из корзины
+    var id = $(this).attr('data-id');
+    cart[id]++;
+    saveCart();
+    showCart();
+};
 
 function saveCart() {
     //сохраняю корзину в localStorage
     localStorage.setItem('cart', JSON.stringify(cart)); //корзину в строку
-}
+};
 
 function isEmpty(object) {
     //проверка корзины на пустоту
